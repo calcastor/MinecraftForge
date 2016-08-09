03d7f9fe50188460c7223bd8f144e90cfa7f9782
LexManos <LexManos@gmail.com>
2016-08-08 17:35:15 -0700

    Compleetly skip FML handshake for Vanilla connections. Should fix race condition. Closes #3084

--- src/main/java/net/minecraftforge/fml/common/network/handshake/NetworkDispatcher.java
+++ src/main/java/net/minecraftforge/fml/common/network/handshake/NetworkDispatcher.java
@@ -128,12 +128,12 @@ public class NetworkDispatcher extends SimpleChannelInboundHandler<Packet> imple
     public void serverToClientHandshake(EntityPlayerMP player)
     {
         this.player = player;
-        insertIntoChannel();
         Boolean fml = this.manager.channel().attr(NetworkRegistry.FML_MARKER).get();
         if (fml != null && fml.booleanValue())
         {
             //FML on client, send server hello
             //TODO: Make this cleaner as it uses netty magic 0.o
+            insertIntoChannel();
         }
         else
         {
