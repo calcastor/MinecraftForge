ab04c5facddd46c26e71af6482b39844cdb652a0
Aaron Hill <aa1ronham@gmail.com>
2017-02-23 18:20:05 -0500

    Add a null check for the throwable message in NetWorkDispatcher#exceptionCaught (#3688)
    (cherry picked from commit 1be1bb59c5c4c7432c515ef07beb6bf7e1f6ef27)

--- src/main/java/net/minecraftforge/fml/common/network/handshake/NetworkDispatcher.java
+++ src/main/java/net/minecraftforge/fml/common/network/handshake/NetworkDispatcher.java
@@ -564,7 +564,7 @@ public class NetworkDispatcher extends SimpleChannelInboundHandler<Packet> imple
         if (!(cause instanceof ClosedChannelException))
         {
             // Mute the reset by peer exception - it's disconnection noise
-            if (cause.getMessage().contains("Connection reset by peer"))
+            if (cause.getMessage() != null && cause.getMessage().contains("Connection reset by peer"))
             {
                 FMLLog.log(Level.DEBUG, cause, "Muted NetworkDispatcher exception");
             }
