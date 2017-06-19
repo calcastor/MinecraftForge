0fb3e55586dc527e06b1422cad633c1b9808f2d7
Lumien <lumien231@users.noreply.github.com>
2017-06-20 01:47:34 +0200

    [1.12] Fixes Exception when getting modlist for a vanilla client (#4028)

--- src/main/java/net/minecraftforge/fml/common/network/handshake/NetworkDispatcher.java
+++ src/main/java/net/minecraftforge/fml/common/network/handshake/NetworkDispatcher.java
@@ -92,7 +92,7 @@ public class NetworkDispatcher extends SimpleChannelInboundHandler<Packet> imple
     private final EmbeddedChannel handshakeChannel;
     private NetHandlerPlayServer serverHandler;
     private INetHandler netHandler;
-    private Map<String,String> modList;
+    private Map<String,String> modList = Collections.emptyMap();
     private int overrideLoginDim;
 
     public NetworkDispatcher(NetworkManager manager)
