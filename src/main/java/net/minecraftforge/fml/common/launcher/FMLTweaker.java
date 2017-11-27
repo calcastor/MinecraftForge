156a9757411975e8b17d20a44c0713d08f29a2fa
Ben Staddon <bs2609@users.noreply.github.com>
2017-11-27 03:20:01 +0000

    Don't force IPv4 network stack if IPv6 is specifically requested (#4547)

--- src/main/java/net/minecraftforge/fml/common/launcher/FMLTweaker.java
+++ src/main/java/net/minecraftforge/fml/common/launcher/FMLTweaker.java
@@ -28,8 +28,10 @@ public class FMLTweaker implements ITweaker {
 
     public FMLTweaker()
     {
-        System.setProperty("java.net.preferIPv4Stack", "true"); //Lets do this as early as possible. Vanilla does it in Main.main
-        System.setProperty("io.netty.noPreferDirect", "true");
+        if (System.getProperty("java.net.preferIPv4Stack") == null)
+        {
+            System.setProperty("java.net.preferIPv4Stack", "true");
+        }
         try
         {
             System.setSecurityManager(new FMLSecurityManager());
